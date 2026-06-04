c0200f1df47b9468cfcf0080930036c04fe868ce
ramesh <rameshkumar.v@celestialsys.com>
2026-06-04 09:40:18 +0530

    added diffrent modules and names

--- modules.py
+++ modules.py
@@ -1,3 +1,19 @@
 import math
 
 print(math.sqrt(25))
+
+from math import sqrt
+
+print(sqrt(25))
+
+from math import sqrt, factorial
+print(factorial(5))
+
+from math import * # Not recomendable
+
+
+#alias import
+
+import math as m
+
+print(m.sqrt(25))
