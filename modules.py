a1ee6358e59b0c9785f94ddd6da904ea8a01f88c
ramesh <rameshkumar.v@celestialsys.com>
2026-06-04 12:08:35 +0530

    add default module sin python

--- modules.py
+++ modules.py
@@ -17,3 +17,20 @@ from math import * # Not recomendable
 import math as m
 
 print(m.sqrt(25))
+
+
+import random
+
+print(random.randint(1,10))
+
+import random
+
+print(random.randint(1,10))
+
+from datetime import datetime
+
+print(datetime.now())
+
+import os
+
+print(os.getcwd())
