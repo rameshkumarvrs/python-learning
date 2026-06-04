7f9711dadb9dc50a83799959e25638aa2f2720fa
ramesh <rameshkumar.v@celestialsys.com>
2026-06-04 15:15:30 +0530

    added student class objects

--- classes.py
+++ classes.py
@@ -14,3 +14,20 @@ class Student:
         
 student1 = Student()
 student2 = Student()
+
+#initalize student attributes name and age
+
+class Student:
+   def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+student1 = Student("ramesh", 35)
+student2 = Student("haran", 4)
+
+print(student1.name)
+print(student1.age)
+
+print(student2.name)
+print(student2.age)
+        
