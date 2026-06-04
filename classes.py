41f1a0041b3f81699a4853dcde698bdcf3d3d538
ramesh <rameshkumar.v@celestialsys.com>
2026-06-04 15:10:18 +0530

    added initializers and self in class

--- classes.py
+++ classes.py
@@ -5,3 +5,12 @@ class Student:
 
 student1 = Student()
 student1.greet()
+
+#initializers in python
+
+class Student:
+   def __init__(self):
+        print("object created")
+        
+student1 = Student()
+student2 = Student()
