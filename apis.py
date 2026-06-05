da0c9a51b6b59faeba340af41e2b3bbb4ce96607
ramesh <rameshkumar.v@celestialsys.com>
2026-06-05 11:14:49 +0530

    added post request with the converted dictionary to json formates

--- apis.py
+++ apis.py
@@ -6,5 +6,38 @@ response = requests.get(url)
 
 data = response.json()
 
-print(data["title"])
+#print(data["title"])
+
+#https://api.example.com/search?q=python
+
+
+# url = "https://api.example.com/search"
+
+# params = {
+# 	"q":"python"
+# }
+
+# response = requests.get(url,params=params)
+
+#print(response.text)
+
+#post api
+
+data = {
+	"name": "ramesh",
+	"age":25
+}
+
+url = "https://httpbin.org/post"
+
+response = requests.post(
+
+     url, 
+     json=data
+ 
+	)
+
+print(response.text)
+
+
 
