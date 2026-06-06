0542a91330f246bbed243d8300318982c1d54824
ramesh <rameshkumarvrs@gmail.com>
2026-06-06 16:53:05 +0530

    mno methods examples zeros ones arange, full, linspace

--- numpy_examples.py
+++ numpy_examples.py
@@ -12,6 +12,23 @@ print(twod_ary)
 
 #np zeros
 
-ze = np.zeros([3,5])
+ze = np.zeros([3,5], dtype='int32')
 
 print(ze)
+
+n3 = np.ones([4,3,3], dtype='int32')
+
+print(n3)
+
+n4 = np.arange(0,8)
+
+print(n4)
+
+n5 = np.linspace(0,20,num=5, dtype='int32')
+
+print(n5)
+
+n6 = np.full([2,5],75)
+
+
+print(n6)
