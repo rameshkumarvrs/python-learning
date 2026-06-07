2e80d5d9a5f022794c7bfd048f91c3270fca4b91
ramesh <rameshkumarvrs@gmail.com>
2026-06-07 23:09:37 +0530

    added dynamic column and rown in pandas

--- python_dataframes_example.py
+++ python_dataframes_example.py
@@ -6,6 +6,25 @@ students = {
 	"CGPA":[8,9,10,9.5]
 }
 
-df = pd.DataFrame(students)
+df = pd.DataFrame(students, index=["ochaye", "pachaye", "pochaye", "supaye"])
+
+print(df)
+
+print(df.loc["pachaye"])
+
+print(df.iloc[1])
+
+df["medium"] = ["Tamil", "English", "english", "English"]
+
+new_student = {
+	"name": "pecha",
+	"age":20,
+	"CGPA":9,
+	"medium":"tamil"
+}
+
+std4 = pd.DataFrame(new_student, index=["apsulkaro"])
+
+df = pd.concat([df, std4])
 
 print(df)
